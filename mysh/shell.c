cadeb7648bcbc4977e2ab56a8bdae27effc16bbe
shahinjowkar <shahin.jowkardris@mail.mcgill.ca>
2025-10-05 14:13:24 -0400

    getToken itter1 done

--- mysh/shell.c
+++ mysh/shell.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define MAXARGS 10
 
@@ -59,26 +62,21 @@ execNode_INIT(){
 
 int
 gettoken(char **start, char *end, char **tok_start, char **tok_end ){
-
 	char *dummy = *start;
 	int tok = 'x';
-	//check if string ended
 	if (dummy >= end){
 		return 'z';
 	}
-	//push pointer up till empty string
-	while(dummy < end && *dummy != ' ' ){
-		dummy++;
+	if(*dummy == '|'){
+		return '|';
 	}
-	//update the start and end of the word by updating **
+
+	while (dummy < end && !isspace(*dummy)) dummy++; //get to end of word
 	if(tok_start){
-		*tok_start = *start;
-		*tok_end = dummy;
+		*tok_start = *start;//set the start
+		*tok_end = dummy;//set the end
 	}
-	//push till next token
-	while(*dummy == ' '){
-		dummy++;
-	}
+	while(dummy<end && isspace(*dummy)) dummy++;//get to next token
 	*start = dummy;
 	return tok;
 }
@@ -92,40 +90,78 @@ parsExec(char *s, size_t size){
 	int counter = 0;
 	execNode *curr = (execNode *) execNode_INIT();
 	while((tok = gettoken(&s,end,&tok_start,&tok_end)) == 'x'){
+		printf("in parsExec : %c\n ", *s);	
 		curr->argv[counter] =  tok_start;
 		curr->eargv[counter] = tok_end;
 		counter++;
 	}
 	curr->argv[counter] = 0;
 	curr->eargv[counter] = 0;
+	printf("s end of parsExec: %c\n ", *s);
 	return (node *)curr;
 }
+
+int 
+peak(char *s, size_t size){
+	char *end = s + size;
+	while(s<end && !isspace(*s)) s++;
+	if(s == end) return 0;
+	if(*s == '|') return '|';
+	else return 'x';
+}
+
+
+node *
+parsePipe(char *s, size_t size){
+	printf("s before parExec(pipe): %c\n ", *s);
+	node *exec1 = parsExec(s,size);
+	printf("s after parsexec(pipe)%c\n", *s);
+	if( peak(s, size) == '|'){
+		printf("PIPE");
+	}
+	printf("NO PIPE");
+
+}
+
+
 void
 parsExecNullTerminate(execNode* curr){
 	for(int i=0; curr->eargv[i]; i++){
-
-		printf("%d\n", i);
-		printf("%c", *(curr->argv[i]));
 		*curr->eargv[i] = 0;
 	}
 }
-void
-printNode(execNode* curr){
-	for(int i=0; curr->argv[i]; i++){
 
-	}
+
+
+void
+exec(execNode* curr){
+	// pid_t pid;
+	// if((pid = fork()) < 0){printf("panik");exit(1)};
+	// if(pid==0){
+	execvp((curr->argv[0]), curr->argv);
+	printf("BAS SYNTAX");exit(0);
+	// }
+
+
 }
 
 int
 main(int argc, char *argv[]){
+
 	while(1){
-		char *buff;
-		size_t cap;
+		char *buff = NULL;
+		size_t cap = 0;
+		pid_t pid;
 		printf("mySh>>" );
 		ssize_t n = getline(&buff, &cap, stdin);
-		node *curr = parsExec(buff,n);
-		printf("-------------\n");
-		parsExecNullTerminate((execNode *) curr);
-		printNode((execNode *) curr);
+		// node *curr = parsExec(buff,n);
+		// parsExecNullTerminate((execNode *) curr);
+		parsePipe(buff,n);
+		// if((pid = fork()) < 0){printf("panik");exit(1);}
+		// if(pid == 0){
+		// 	exec((execNode *) curr);
+		// }
+		// wait(0);
+
 	}
 }
