4d9f67e071f3108f83d9d12e1dec5e0efc7d027f
shahinjowkar <shahin.jowkardris@mail.mcgill.ca>
2025-10-04 23:48:40 -0400

    exec parser

--- mysh/shell.c
+++ mysh/shell.c
@@ -5,20 +5,10 @@
 #define MAXARGS 10
 
 typedef enum {EXEC = 0, REDIR =1 , PIPE = 2, SEQ = 3, BG = 4 } cmdtype_t;
-typedef enum{INPUT = 0, OUTPU =1} redir_t;
-
-
 
 typedef struct{
 	cmdtype_t type;
 } node;
-
-typedef struct{
-	cmdtype_t type;
-	char *argv[MAXARGS];
-	char *eargv[MAXARGS];
-
-}execNode;
 
 typedef struct{
 	cmdtype_t type;
@@ -35,35 +25,97 @@ typedef struct{
 typedef struct{
 	cmdtype_t type;
 	node *child;
-} BG;
+} bgNode;
 
 typedef struct{
-	cmdtype_t;
-	
+	cmdtype_t type;
+	int fd;
+	int mode;
+	node *child;
+	char *file;
+	char *efile; 
+} redirNode;
+
+
+typedef struct{
+	cmdtype_t type;
+	char *argv[MAXARGS];
+	char *eargv[MAXARGS];
+
+}execNode;
+
+
+
+node *
+execNode_INIT(){
+	execNode *curr;
+	curr = malloc(sizeof(execNode));
+	memset(curr,0,sizeof(execNode));
+	curr->type = EXEC;
+	return (node *) curr;
 }
 
-char
-getToken(char **ps, char *es, ){
-	
+
+
+int
+gettoken(char **start, char *end, char *tok_start, char *tok_end ){
+
+	char *dummy = *start;
+	if(**start == EOF){
+		return 'z';
+	}
+	printf("checkpoint2");
+
+	int tok = 'x';
+	while(dummy < end && strcmp(dummy, " ") != 0){
+		dummy++;
+	}
+	printf("checkpoint3");
+
+	if(tok_start){
+		tok_start = *start;
+	}
+	printf("checkpoint4");
+
+	if(tok_end){
+		tok_end = dummy;
+	}
+	printf("checkpoint5");
+
+	while(strcmp(dummy," ") == 0){
+		dummy++;
+	}
+	printf("checkpoint6");
+
+	*start = dummy; 
+	printf("\n %c \n", tok);
+	return tok;
+
+
 }
 
 
 node*
-parser(char *buff, size_t size){
-	char *s = buff;
-	char *es = buff + size; 
-	getToken(&s,es);
-};
+parsExec(char *s, size_t size){
+	int tok;
+	char *end = s+size;
+	char *tok_start, *tok_end;
+	node *curr = execNode_INIT();
+	printf("checkpoint1");
+	while((tok = gettoken(&s,end,tok_start,tok_end)) == 'x'){
+		printf("HIIII");
+	}
+
+}
 
 int
 main(int argc, char *argv[]){
 	while(1){
-
 		char *buff;
 		size_t size;
 		printf("mySh>>" );
 		getline(&buff, &size, stdin);
-		parser(*buff,size)
+		parsExec(buff,size);
 
 	}
 }
