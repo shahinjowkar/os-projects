5ef8c2deb60c9a6c11a981067b0eb1c1b942b973
shahinjowkar <shahin.jowkardris@mail.mcgill.ca>
2025-10-07 00:30:11 -0400

    redirect and chaining added

--- mysh/shell.c
+++ mysh/shell.c
@@ -9,6 +9,7 @@
 
 typedef enum {EXEC = 0, REDIR =1 , PIPE = 2, SEQ = 3, BG = 4 } cmdtype_t;
 
+
 //structs
 typedef struct{
 	cmdtype_t type;
@@ -66,12 +67,25 @@ pipeNode_INIT(){
 	return (node *) curr;
 }
 
+node *
+redirNode_INIT(){
+	redirNode *curr;
+	curr = malloc(sizeof(redirNode));
+	memset(curr,0,sizeof(redirNode));
+	curr->type = REDIR;
+	return (node *) curr;
+}
 //PROCESSORS
 int 
 peak(char *s, char *end){
 	while(s<end && isspace(*s)) s++;
 	if(s == end) return 0;
 	if(*s == '|') return '|';
+	if(*s == '<') return '<';
+	if(*s == '>'){
+		if(s+1<end && *(s+1) == '>'){return '+';}
+		else{return '>';}
+	}
 	else return 'x';
 }
 
@@ -79,11 +93,12 @@ int
 gettoken(char **start, char *end, char **tok_start, char **tok_end ){
 	char *dummy = *start;
 	int tok = 'x';
-	if (dummy >= end){
-		return 0;
-	}
-	if(*dummy == '|'){
-		tok = '|';
+	if (dummy >= end){return 0;}
+	if(*dummy == '|'){tok = '|';}
+	if(*dummy == '<'){tok = '<';}
+	if(*dummy == '>'){
+		if(dummy + 1 < end && *(dummy +1) == '>'){tok = '+';}
+		else{tok =  '>';}
 	}
 
 	while (dummy < end && !isspace(*dummy)) dummy++; //get to end of word
@@ -105,6 +120,51 @@ parsExecNullTerminate(execNode* curr){
 }
 
 //PARSERS
+
+node *
+parseRedir(node *child, char **s, char *end){
+	int tok;
+	char *tok_start, *tok_end;
+	redirNode *curr = (redirNode *)redirNode_INIT();
+	tok = gettoken(s,end,0,0);
+
+	if(tok == '<'){
+		curr->fd = STDIN_FILENO;
+		curr->mode = O_RDONLY;
+	}
+	else if(tok == '>'){
+		curr -> fd = STDOUT_FILENO;
+		curr->mode = O_WRONLY | O_CREAT | O_TRUNC;
+	}
+	else if(tok == '+'){
+		curr->fd = STDOUT_FILENO;
+		curr->mode =O_WRONLY | O_CREAT | O_APPEND;
+	}
+	else{
+		free(curr);
+		exit(1);
+	}
+	
+	if((tok = gettoken(s,end,&tok_start,&tok_end)) != 'x'){
+
+		exit(1);
+	}
+	curr->efile = tok_end;
+	curr->file = tok_start;
+	if(peak(*s,end) == 0){
+		curr->child = child;
+		return (node *) curr;
+	}
+	if(peak(*s,end) == '>' || peak(*s,end) == '<' || peak(*s,end) == '+' ){
+			curr->child = (node *) parseRedir(child,s,end);
+	}
+	else{
+		curr->child = child;
+	}
+	return (node *)curr;	
+}
+
+
 node*
 parsExec(char **s, char *end){
 	int tok;
@@ -119,23 +179,24 @@ parsExec(char **s, char *end){
 	}
 	curr->argv[counter] = 0;
 	curr->eargv[counter] = 0;
+	if((peak(*s,end) == '<') || (peak(*s,end) == '+') ||(peak(*s,end) == '>')){
+		return parseRedir((node *)curr,s,end);
+	}
 	return (node *)curr;
 }
 
 
 node *
 parsePipe(char *s, char *end){
+	// printf("PARSING PIPE\n");
 	node *exec1 = parsExec(&s,end);
 	if(peak(s,end) == '|'){
 		gettoken(&s,end,0,0);
 		pipeNode *curr = (pipeNode *) pipeNode_INIT();
 		curr->left = exec1;
 		curr->right = parsePipe(s,end);
-		parsExecNullTerminate((execNode *) curr->left);
-		parsExecNullTerminate((execNode *) curr->right);
 		return (node *) curr;
 	}
-	parsExecNullTerminate((execNode *) exec1);
 	return exec1;
 
 }
@@ -170,6 +231,17 @@ exec(node* curr){
 		wait(NULL);
 		wait(NULL);
 	}
+	if(curr->type == REDIR){
+
+		int fdOld = ((redirNode *) curr) ->fd;
+		int mode = ((redirNode *) curr) -> mode;
+		node *child = ((redirNode *) curr) -> child;
+		char *file = ((redirNode *) curr) -> file;
+		int fd = open(file,mode,S_IRWXU);
+		printf("fdOld: %d, mode: %d, fileName: %s\n", fdOld,mode,file);
+		dup2(fd,fdOld);
+		exec(child);
+	}
 	if(curr->type == EXEC){
 		execvp((((execNode *)curr)->argv[0]), ((execNode *)curr)->argv);
 		printf("BAS SYNTAX");exit(0);
@@ -181,12 +253,19 @@ exec(node* curr){
 
 void
 printTree(node *curr){
-	if(curr->type == PIPE){
+	if(curr->type == REDIR){
+		*(((redirNode *) curr)->efile) = '\0';
+		printf("%s\n", ((redirNode *) curr)->file);
+		printTree((((redirNode *) curr)->child));
+	}
+	else if(curr->type == PIPE){
+		printf("|\n");
 		printTree(((pipeNode *) curr) ->left);
 		printTree(((pipeNode *) curr) ->right);
 	}
 	else{
-		for(int i = 0; ((execNode *) curr)->argv[i];i++) printf("left: %s\n", ((execNode *) curr)->argv[i]);
+		parsExecNullTerminate((execNode *) curr);
+		for(int i = 0; ((execNode *) curr)->argv[i];i++) printf("%s\n", ((execNode *) curr)->argv[i]);
 	}
 }
 void 
@@ -196,7 +275,17 @@ printExec(execNode *curr){
 
 int
 main(int argc, char *argv[]){
+		// char *buff = NULL;
+		// size_t cap = 0;
+		// pid_t pid;
+		// printf("mySh>>" );
+		// ssize_t n = getline(&buff, &cap, stdin);
+		// char *end = buff + n;
+		// node *root = parsePipe(buff,end);
+		// printTree(root);
+		// exec(root);
 
+	
 	while(1){
 		char *buff = NULL;
 		size_t cap = 0;
@@ -206,7 +295,9 @@ main(int argc, char *argv[]){
 		char *end = buff + n;
 		if((pid = fork()) < 0){printf("panik7");exit(1);}
 		if(pid == 0){
-			exec(parsePipe(buff,end));
+			node *root = parsePipe(buff,end);
+			printTree(root);
+			exec(root);
 		}
 		wait(NULL);
 		
